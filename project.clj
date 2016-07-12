b2e6024e0590595e16bf321916a1438b45e31b8d
Emil Segerbäck <emilsegerback@gmail.com>
2016-07-12 08:03:21 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject mixer "0.1.0-SNAPSHOT"
+  :description "A simple music mixer"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.89"]
+                 [org.omcljs/om "1.0.0-alpha38"]
+                 [figwheel-sidecar "0.5.4-7" :scope "test"]])
