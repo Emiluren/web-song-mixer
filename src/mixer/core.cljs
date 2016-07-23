488c0fb5b208cf329abad9405f38b6dc6f38d646
Emil Segerbäck <emilsegerback@gmail.com>
2016-07-23 16:43:50 +0200

    Fix wait for song loading

--- src/mixer/core.cljs
+++ src/mixer/core.cljs
@@ -5,38 +5,61 @@
             [ajax.core :refer [GET]]
             [sablono.core :as html :refer-macros [html]]))
 
-(defn load-track [track]
-  (let [name (get track "name")
-        files (map #(str "songs/" %) (get track "files"))]
-    {:name name :audio (js/Howl. #js {:src (into-array files)})}))
+(defn fractions [values]
+  (let [total (count values)
+        kval-to-kfrac (fn [[k v]]
+                        [k (* (/ 1 total) v)])]
+    (into {} (map kval-to-kfrac (frequencies values)))))
+
+(defn show-load-error [files name id msg]
+  (js/alert (str "Kunde inte läsa in filerna "
+                 files " för spåret " name "("
+                 id ", " msg ")")))
 
 (defonce app-state (atom {}))
+
+(defn load-track [track]
+  (let [name (get track "name")
+        files (map #(str "songs/" %) (get track "files"))
+        options #js {:src (into-array files)
+                     :onloaderror #(show-load-error files name %1 %2)
+                     :onload #(swap! app-state assoc-in
+                                     [:finished-loading name] true)}]
+    {:name name :audio (js/Howl. options)}))
+
 
 (defn songs-handler [songs]
   (swap! app-state assoc :songs songs))
 
 (GET "songs.json" {:handler songs-handler})
 
-(defn on-click-play [e]
+(defn play-tracks! []
   (doseq [track (:loaded-tracks @app-state)]
-    (.play (:audio track)))
-  #_(swap! app-state assoc :playid
-         (.play (:song @app-state))))
+    (.play (:audio track))))
 
-(defn on-click-pause [e]
+(defn pause-tracks! []
   (doseq [track (:loaded-tracks @app-state)]
     (.pause (:audio track))))
 
-(defn on-click-stop [e]
+(defn stop-tracks! []
   (doseq [track (:loaded-tracks @app-state)]
     (.stop (:audio track))))
 
-(defn on-song-click [song]
-  (on-click-stop nil)
+(defn loaded-false [tracks]
+  (let [false-vec (fn [name] [name false])
+        track-names (map #(get % "name") tracks)]
+    (into {} (map false-vec track-names))))
+
+(defn reset-finished-loading! [tracks]
+  (swap! app-state assoc :finished-loading (loaded-false tracks)))
+
+(defn load-new-song! [song]
+  (stop-tracks!)
   (let [tracks (get song "tracks")]
+    (reset-finished-loading! tracks)
     (swap! app-state assoc
-           :loaded-song song
-           :loaded-tracks (map load-track tracks))))
+           :selected-song song
+           :loaded-tracks (doall (map load-track tracks)))))
 
 (defn mutate [{:keys [state] :as env} key params]
   (if (= 'increment key)
@@ -58,42 +81,47 @@
    [:label " Balans: "
     [:input {:type "range" :min -100 :max 100 :default-value 0}]]])
 
-(defn create-song-ui [song]
-  (if (every? #(= "loaded" (.state (:audio %))) (get :loaded-tracks @app-state))
+(defn create-song-ui [song finished-loading]
+  (if (every? identity (vals finished-loading))
     [:div
      [:div
-      [:button {:onClick on-click-play} "Play"]
-      [:button {:onClick on-click-pause} "Pause"]
-      [:button {:onClick on-click-stop} "Stop"]]
+      [:button {:onClick play-tracks!} "Play"]
+      [:button {:onClick pause-tracks!} "Pause"]
+      [:button {:onClick stop-tracks!} "Stop"]]
 
      (into [:div [:h3 (get song "title")]]
            (map create-track-ui (get song "tracks")))]
-    "Ljudfiler laddas in"))
+    (str "Ljudfiler laddas in " (-> (fractions (vals finished-loading))
+                                    (get true)
+                                    (* 100)
+                                    int)
+         "%.")))
 
 (defn create-song-list [songs]
   (map
    (fn [song]
      [:li {:key (get song "title")}
       [:a
-           {:onClick (fn [e] (on-song-click song)) :href "#"}
+           {:onClick (fn [e] (load-new-song! song)) :href "#"}
            (get song "title")]])
    songs))
 
 (defn generated-html [component]
-  (let [{:keys [loaded-song songs]} (om/props component)]
+  (let [{:keys [selected-song songs finished-loading]} (om/props component)]
     (html [:div
+           #_(str finished-loading)
            (if (= :not-found songs)
              "Laddar ner låtlista"
              [:ul (create-song-list songs)])
 
-           (if (= :not-found loaded-song)
+           (if (= :not-found selected-song)
              "Klicka på en låt i listan"
-             (create-song-ui loaded-song))])))
+             (create-song-ui selected-song finished-loading))])))
 
 (defui Mixer
   static om/IQuery
   (query [this]
-         [:loaded-song :songs])
+         [:selected-song :songs :finished-loading])
   Object
   (render [this]
             (generated-html this)))
