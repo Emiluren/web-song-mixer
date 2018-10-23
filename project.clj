fe13a29d8fcef7bf9171931d80e389dcf1de5b43
Emil Segerbäck <emilsegerback@mykolab.ch>
2018-10-23 21:45:06 +0200

    Update dependencies and workflow

--- project.clj
+++ project.clj
@@ -1,8 +1,21 @@
 (defproject mixer "0.1.0-SNAPSHOT"
   :description "A simple music mixer"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.89"]
-                 [org.omcljs/om "1.0.0-alpha38"]
-                 [figwheel-sidecar "0.5.4-7" :scope "test"]
-                 [cljs-ajax "0.5.8"]
-                 [sablono "0.7.2"]])
+
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.339"]]
+
+  :source-paths ["src"]
+
+  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
+            "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]}
+
+
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
+                                  [cider/piggieback "0.3.10"]
+                                  [org.omcljs/om "1.0.0-beta1"]
+                                  [cljs-ajax "0.7.3"]
+                                  [sablono "0.8.4"]]}
+             :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
+             }
+  )
---
+++ test/mixer/core_test.cljs
@@ -0,0 +1,10 @@
+(ns mixer.core-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]
+     [mixer.core :refer []]))
+
+#_(deftest multiply-test
+  (is (= (* 1 2) (multiply 1 2))))
+
+#_(deftest multiply-test-2
+  (is (= (* 75 10) (multiply 10 75))))
